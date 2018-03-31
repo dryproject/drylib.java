c763824e986e6f30871ffceeef57f5f8a0c2d187
Arto Bendiken <arto@dryproject.org>
2018-03-31 16:47:14 +0300

    Added unit test skeletons for current classes.

---
+++ src/test/java/dry/TestWord32.java
@@ -0,0 +1,15 @@
+/* This is free and unencumbered software released into the public domain. */
+
+package dry;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
+
+class TestWord32 {
+
+  @Test
+  void test() {
+    assertThat(42).isEqualTo(42);
+  }
+}
