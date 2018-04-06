96f98ec607123bfe5658639721cd746e0fc14fd8
Arto Bendiken <arto@dryproject.org>
2018-04-06 16:08:37 +0300

    Defined Interval.

---
+++ src/main/java/dry/Interval.java
@@ -0,0 +1,14 @@
+/* This is free and unencumbered software released into the public domain. */
+
+package dry;
+
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * An interval.
+ *
+ * @see <a href="https://en.wikipedia.org/wiki/Interval_(mathematics)">[1]</a>
+ */
+public interface Interval<E> extends Datum {
+  // TODO
+}
