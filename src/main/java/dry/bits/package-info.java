e0778bfdce8eaa9bc69f33603af70c810800462b
Arto Bendiken <arto@dryproject.org>
2018-06-06 16:43:12 +0300

    Defined dry.bits and dry.logic.

---
+++ src/main/java/dry/bits/package-info.java
@@ -0,0 +1,8 @@
+/* This is free and unencumbered software released into the public domain. */
+
+/**
+ * DRYlib for Java: the <tt>dry/bits</tt> module.
+ *
+ * @see <a href="https://drylib.org/bits/">[1]</a>
+ */
+package dry.bits;
---
+++ src/main/java/dry/logic/package-info.java
@@ -0,0 +1,8 @@
+/* This is free and unencumbered software released into the public domain. */
+
+/**
+ * DRYlib for Java: the <tt>dry/logic</tt> module.
+ *
+ * @see <a href="https://drylib.org/logic/">[1]</a>
+ */
+package dry.logic;
