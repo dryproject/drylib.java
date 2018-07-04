bc4a77f159136f6be3ecaba7a73f985c4cf4b3ec
Arto Bendiken <arto@dryproject.org>
2018-07-04 08:18:04 +0300

    Defined test suites for strings.

---
+++ src/test/java/dry/text/TestASCII.java
@@ -0,0 +1,15 @@
+/* This is free and unencumbered software released into the public domain. */
+
+package dry.text;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
+
+class TestASCII {
+
+  @Test
+  void testConstructWithNull() {
+    //assertThatNullPointerException().isThrownBy(() -> { String.of((Char)null); });
+  }
+}
